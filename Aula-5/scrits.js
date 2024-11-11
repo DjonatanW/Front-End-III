02abf7a3fcdb2bb1c47088ded8aea29e46e423d9
Djonatan Weber <djonatan11021996@gmail.com>
2024-11-11 20:34:29 -0300

    feat: class 5

---
+++ Aula-5/scrits.js
@@ -0,0 +1,18 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+const paginationItems = document.querySelectorAll('.pagination.page-item')
+
+paginationItems.forEach(item => {
+  item.addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    paginationItems.forEach(item => {
+      item.classList.remove('active')
+    })
+    
+    
+    item.classList.add('active')
+  })
+})
