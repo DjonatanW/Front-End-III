3216f518b0461dda9c5180f92cdd92d3aaa743bf
Djonatan Weber <djonatan11021996@gmail.com>
2024-11-11 22:30:44 -0300

    feat: activity for using bootstrap.

--- Aula-4/Activity-project-list-message-bootstrap/front-recados/src/js/scripts.js
+++ Aula-4/Activity-project-list-message-bootstrap/front-recados/src/js/scripts.js
@@ -29,15 +29,43 @@ async function fetchNotes(page) {
     notes.forEach((note) => {
       const noteCard = document.createElement('div')
       noteCard.classList.add('card')
-
+      noteCard.classList.add('mb-3')
+      alert("aqui")
+      
       noteCard.innerHTML = `
         <h3 class="card-title">${note.title}</h3>
         <p class="card-description">${note.description}</p>
 
-        <div class="card-icons">
-          <i class="bi bi-trash" data-id=${note.id}></i>
-          <i class="bi bi-pencil" data-id=${note.id}></i>
+        <div class="card-icons mb-2 b-4">
+          <i class="bi bi-trash p-2 fs-4" data-id=${note.id}></i>
+          <i class="bi bi-pencil p-2 fs-4" data-bs-toggle="modal" data-bs-target="#form-edit-note" data-id=${note.id}></i>
         </div>
+          <div class="modal fade" id="form-edit-note" tabindex="-1" aria-labelledby="form-edit-note" aria-hidden="true">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-body">
+                  <img src="src/images/logo.svg" alt="" class="logo" />
+
+                  <h1 class="title">Edite seu recado</h1>
+                  <h2 class="subtitle">Você pode alterar um recado já cadastrado</h2>
+                  <form id="form-edit-note">
+                    <div class="form-control">
+                      <label for="title-edit">Título</label>
+                      <input type="text" id="title-edit" placeholder="Insira um título para o recado" />
+                    </div>
+
+                    <div class="form-control">
+                      <label for="description-edit">Descrição</label>
+                      <textarea id="description-edit"></textarea>
+                    </div>
+
+                    <button class="btn-submit">Atualizar recado</button>
+                    <button class="btn-submit" type="reset">Limpar campos</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
       `
       
       notesContainer.appendChild(noteCard)
@@ -74,21 +102,32 @@ function navigateToEditPage(noteId) {
 
 fetchNotes(currentPage)
 
-prevPage.addEventListener('click', () => {
+prevPage.addEventListener('click', (event) => {
+  event.preventDefault()
+
   if (currentPage > 1) {
     currentPage--
-
+    nextPage.classList.remove('active')
+    prevPage.classList.remove('disabled')
+    prevPage.classList.add('active')
+    
     fetchNotes(currentPage)
   }
+  prevPage.classList.remove('disabled')
 
 })
 
-nextPage.addEventListener('click', () => {
+nextPage.addEventListener('click', (event) => {
+  event.preventDefault()
+
   if (currentPage < totalPages) {
     currentPage++
-
+    prevPage.classList.remove('active')
+    nextPage.classList.remove('disabled')
+    nextPage.classList.add('active')
     fetchNotes(currentPage)
   }
+  nextPage.classList.remove('disabled')
 })
 
 function updatePaginationButtons() {
